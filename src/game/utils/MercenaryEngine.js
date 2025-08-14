6cba05ac4c1d1a2c5e00020c1a447f8fa616a1fb
cagecorn <lmcbv@naver.com>
2025-08-14 21:32:36 +0900

    feat: filter mbti by class

--- src/game/utils/MercenaryEngine.js
+++ src/game/utils/MercenaryEngine.js
@@ -9,6 +9,8 @@ import { attributeSpecializations } from '../data/attributeSpecializations.js';
 import { diceEngine } from './DiceEngine.js';
 // ✨ [추가] 아키타입 결정 엔진을 가져옵니다.
 import { archetypeAssignmentEngine } from './ArchetypeAssignmentEngine.js';
+// ✨ 클래스별 MBTI 필터링 엔진을 가져옵니다.
+import { classMBTIFilterEngine } from './ClassMBTIFilterEngine.js';
 
 /**
  * 용병의 생성, 저장, 관리를 전담하는 엔진 (싱글턴)
@@ -46,6 +48,9 @@ class MercenaryEngine {
         const randomName = this.mercenaryNames[Math.floor(Math.random() * this.mercenaryNames.length)];
         const uniqueId = uniqueIDManager.getNextId();
 
+        // ✨ 클래스에 따라 허용된 MBTI 성향을 우선적으로 부여합니다.
+        const filteredMbti = classMBTIFilterEngine.generateFor(baseMercenaryData.id);
+
         const newInstance = {
             ...baseMercenaryData,
             uniqueId,
@@ -56,7 +61,7 @@ class MercenaryEngine {
             // ✨ 모든 용병이 동일한 형태의 스킬 슬롯을 갖도록 초기화합니다.
             skillSlots: [null, null, null, null, null, null, null, null],
             // ✨ 각 용병은 고유한 MBTI 성향을 가집니다.
-            mbti: this._generateMBTI()
+            mbti: filteredMbti || this._generateMBTI()
         };
 
         // ✨ [핵심 추가] 용병 생성 직후 아키타입을 결정합니다.
